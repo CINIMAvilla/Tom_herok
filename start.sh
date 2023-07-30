1eb883c5c8e3dac9f901f82141ab336eeb34b67e
CINIMAvilla <126217261+CINIMAvilla@users.noreply.github.com>
2023-07-30 19:13:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/CINIMAvilla/Tom_herok/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/CINIMAvilla/Tom_herok.git /Tom_herok
+cd /Tom_herok
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
