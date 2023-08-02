9276c6701979e9ff4bb5f341e5a8809dd28de57b
CINIMAvilla <126217261+CINIMAvilla@users.noreply.github.com>
2023-08-02 06:59:45 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/CINIMAvilla/Tom_herok.git /Tom_herok
-cd /Tom_herok
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 bot.py
